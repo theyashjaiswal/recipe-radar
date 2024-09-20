234df79231ff0c385541c0b8d668f7a0d69081d8
YASH <yashjaiswalofficial@gmail.com>
2024-09-20 03:04:37 -0300

    added theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { ThemeProvider } from "../src/components/ui/theme-provider";
@@ -5,17 +6,38 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import NotFoundPage from "./NotFound";
 import RecipeDetailPage from "./RecipeDetailPage";
+import { useEffect, useState } from "react";
 
 function App() {
   // const [showSidebar, onSetShowSidebar] = useState(false);
   // const { setTheme } = useTheme();
+  const [theme, setTheme] = useState("dark");
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  function toggleTheme(_theme: any) {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  }
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/recipeDetailPage/:id" element={<RecipeDetailPage />} />
+          <Route
+            path="/"
+            element={<HomePage theme={theme} toggleTheme={toggleTheme} />}
+          />
+          <Route
+            path="/recipeDetailPage/:id"
+            element={
+              <RecipeDetailPage theme={theme} toggleTheme={toggleTheme} />
+            }
+          />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Router>
--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 // import { useTheme } from "./components/ui/theme-provider";
@@ -7,12 +8,12 @@ import { Navbar } from "./Navbar";
 
 import { SearchBox } from "./SearchBox";
 
-const HomePage = () => {
-  // const { setTheme } = useTheme();
+const HomePage = (props: any) => {
+  const { theme, toggleTheme } = props;
 
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar theme={theme} toggleTheme={toggleTheme}></Navbar>
       <div className="mt-2 bg-white dark:bg-gray-950 max-w-6xl mx-auto pb-12 pt-2 sm:pb-24 sm:pt-2 px-">
         <div className="mt-20 flex flex-col gap-4 p-6 m-6">
           <h2 className="text-left text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] dark:bg-gradient-to-b dark:from-[#f9fafb] dark:to-[#aa6262] text-transparent dark:text-transparent bg-clip-text mt-5">
